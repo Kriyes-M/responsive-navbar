1543fcd2850df3ff5f4756c96bd62a0c90eba9d5
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-08 21:31:23 +0000

    Add toggle links function

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const hamburgerButton = document.getElementById('hamburgerButton');
 const linksList = document.getElementById('linksList');
+
+function toggleLinks() {
+  if (linksList.classList.contains('show-links')) {
+    linksList.classList.remove('show-links');
+  }
+  else {
+    linksList.classList.add('show-links');
+  }
+}
